e3274337304bb2e8f2c999ae4e721194c63d826c
aleksey <agat00@gmail.com>
2022-07-01 22:11:28 +0300

    liquidationBot/reporting/reporterRunner - fix a bunch of bugs
    * replace recursion with a loop in order to prevent potential stack overflow
    * add ts-ignore for Error.cause - the TS version is still too low to have types for it
    * ensure that reporter would stop when bot would be stopped

--- src/liquidationBot/reporting/reporterRunner.ts
+++ src/liquidationBot/reporting/reporterRunner.ts
@@ -4,26 +4,30 @@ export async function runReporter(
   reporter: Reporter,
   reportable: Reportable
 ): Promise<void> {
-  try {
-    // potentially reporters may also export async init() function.
-    // Run it here and in catch below, above the reportEvent call
-    for await (const event of reportable.getEventsIterator()) {
-      await reporter.reportEvent(event);
-    }
-  } catch (error: unknown) {
-    let reporterCrashError;
-    if (error instanceof Error) {
-      reporterCrashError = Error("Reporter will be restarted after crash", {
-        cause: error,
-      });
-    } else {
-      reporterCrashError = Error(
-        "Reporter will be restarted after crash\n"
-        + `Nested error: ${error}`
-      );
-    }
-    await reporter.reportEvent({ type: "error", error: reporterCrashError });
+  while (true) {
+    try {
+      // potentially reporters may also export async init() function.
+      // Run it here and in catch below, above the reportEvent call
+      for await (const event of reportable.getEventsIterator()) {
+        await reporter.reportEvent(event);
 
-    return runReporter(reporter, reportable);
+        if (event.type == "botStopped") {
+          return;
+        }
+      }
+    } catch (error: unknown) {
+      let reporterCrashError;
+      if (error instanceof Error) {
+        // @ts-ignore - remove ignore after bump of TS version
+        reporterCrashError = Error("Reporter will be restarted after crash", {
+          cause: error,
+        });
+      } else {
+        reporterCrashError = Error(
+          "Reporter will be restarted after crash\n" + `Nested error: ${error}`
+        );
+      }
+      await reporter.reportEvent({ type: "error", error: reporterCrashError });
+    }
   }
 }
